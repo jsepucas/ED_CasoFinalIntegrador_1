a51bc6f270ca679f3d9bfb919ec7a37e539915dc
jsepucas <jsepucas@myuax.com>
2024-02-21 11:09:24 +0100

    Commit 1

--- EdD_CasoFinalIntegrador_1/src/main/java/Ejercicio_1.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/Ejercicio_1.java
@@ -4,5 +4,12 @@ public class Ejercicio_1 {
 
         TimeConverter converter = new TimeConverter();
         DataLimits dataLimits = new DataLimits();
+
+        // Un dia en la tierra:
+        double earthSeconds = 86400;
+        double newPlanetSeconds = converter.convertEarthTimeToNewPlanet(earthSeconds);
+        System.out.println("Tiempo en la Tierra (segundos): " + earthSeconds);
+        System.out.println("Equivalente en el nuevo planeta (segundos): " + newPlanetSeconds);
+
     }
 }
