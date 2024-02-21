336453d849dcbcf59379d25801b119b8829aee05
jsepucas <jsepucas@myuax.com>
2024-02-21 11:50:57 +0100

    Commit 3

--- EdD_CasoFinalIntegrador_1/src/main/java/CronometroCósmico/Ejercicio_1.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/CronometroCósmico/Ejercicio_1.java
@@ -5,11 +5,11 @@ public class Ejercicio_1 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenido al Cronómetro Cósmico");
-        System.out.println("Ingrese la duración del día en el nuevo planeta (en horas): ");
+        System.out.println("Te damos la bienvenida al Cronómetro Cósmico");
+        System.out.println("Introduce la duración del día en el nuevo planeta (en horas): ");
         double duracionDiaPlaneta = scanner.nextDouble();
 
-        System.out.println("Ingrese la duración del año en el nuevo planeta (en días): ");
+        System.out.println("Introduce la duración del año en el nuevo planeta (en días): ");
         double duracionAnioPlaneta = scanner.nextDouble();
 
         System.out.println("Ingrese el tiempo en la Tierra (en días): ");
--- EdD_CasoFinalIntegrador_1/src/main/java/RecursosySuministros/Ejercicio_2.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/RecursosySuministros/Ejercicio_2.java
@@ -1,4 +1,9 @@
 package RecursosySuministros;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 public class Ejercicio_2 {
+
+
 }
