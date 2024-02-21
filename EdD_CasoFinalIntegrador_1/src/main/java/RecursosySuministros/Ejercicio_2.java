e952e09f18fa0e283eed707f8a8dce9b975dd590
jsepucas <jsepucas@myuax.com>
2024-02-21 11:51:56 +0100

    Commit 3

--- EdD_CasoFinalIntegrador_1/src/main/java/RecursosySuministros/Ejercicio_2.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/RecursosySuministros/Ejercicio_2.java
@@ -5,5 +5,15 @@ import java.util.ArrayList;
 import java.util.Collections;
 public class Ejercicio_2 {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de periodos a analizar (en múltiplos de 5 días):");
+        int periodos = scanner.nextInt();
+
+        ArrayList<Integer> consumoAgua = new ArrayList<>();
+        ArrayList<Integer> consumoComida = new ArrayList<>();
+        ArrayList<Integer> consumoOxigeno = new ArrayList<>();
+
 
 }
