6be1161d701afd3f7eb2a22c3cbcb016d557cca3
jsepucas <jsepucas@myuax.com>
2024-02-21 11:08:44 +0100

    Commit 1

--- EdD_CasoFinalIntegrador_1/src/main/java/Ejercicio_1.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/Ejercicio_1.java
@@ -3,5 +3,6 @@ public class Ejercicio_1 {
     public static void main(String[] args) {
 
         TimeConverter converter = new TimeConverter();
+        DataLimits dataLimits = new DataLimits();
     }
 }
