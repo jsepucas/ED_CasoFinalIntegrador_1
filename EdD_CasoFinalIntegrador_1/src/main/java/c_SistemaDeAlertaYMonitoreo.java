e6765543fd4f4593dda91fe75a79fd5e2567b121
jsepucas <jsepucas@myuax.com>
2024-02-24 17:09:26 +0100

    Commit

--- EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
@@ -50,3 +50,12 @@ public class c_SistemaDeAlertaYMonitoreo {
             number++;
         }
     }
+
+    private static boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
