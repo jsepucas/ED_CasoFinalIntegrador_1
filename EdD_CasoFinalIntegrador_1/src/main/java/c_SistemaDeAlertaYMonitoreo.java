403378a27add42ea42a1f008b0f5c937ee55a0cf
jsepucas <jsepucas@myuax.com>
2024-02-24 17:05:04 +0100

    Commit

--- EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
@@ -8,6 +8,19 @@ public class c_SistemaDeAlertaYMonitoreo {
         Scanner scanner = new Scanner(System.in);
 
         // Se le preguntan los datos al usuario
-        System.out.print("Ingrese el nivel de radiación: ");
+        System.out.print("Introduce el nivel de radiación: ");
         double radiationLevel = scanner.nextDouble();
-}
+
+        System.out.print("Introdce la temperatura: ");
+        double temperature = scanner.nextDouble();
+
+        System.out.print("Introduce la presión: ");
+        double pressure = scanner.nextDouble();
+
+        // Se verifican las condiciones extremas
+
+        if (radiationLevel > 5) System.out.println("Alerta: El nivel de radiación alto");
+        if (temperature > 30) System.out.println("Alerta: La Temperatura es alta");
+        if (pressure < 960) System.out.println("Alerta: La presión es baja");
+
+    }
