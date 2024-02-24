c8a9f018af659b8fd5d067a7fc66abef1c9d9438
jsepucas <jsepucas@myuax.com>
2024-02-24 17:03:26 +0100

    Commit

--- EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/c_SistemaDeAlertaYMonitoreo.java
@@ -4,5 +4,10 @@ import java.util.Scanner;
 
 public class c_SistemaDeAlertaYMonitoreo {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Se le preguntan los datos al usuario
+        System.out.print("Ingrese el nivel de radiación: ");
+        double radiationLevel = scanner.nextDouble();
 }
