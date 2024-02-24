676bb7ad390318b9608710d1b66b63cdb802617c
jsepucas <jsepucas@myuax.com>
2024-02-24 17:22:45 +0100

    Commit

---
+++ EdD_CasoFinalIntegrador_1/src/test/java/SistemaDeAlertaYMonitoreo_Test.java
@@ -0,0 +1,7 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+
+public class SistemaDeAlertaYMonitoreo_Test {
+}
