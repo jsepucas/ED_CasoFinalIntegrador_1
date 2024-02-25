bce2aa8fe48c66759b609609dd10ae654f69d308
jsepucas <jsepucas@myuax.com>
2024-02-25 19:23:34 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/main/java/f_ComunicadorInterplanetario.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/f_ComunicadorInterplanetario.java
@@ -19,3 +19,31 @@ public class f_ComunicadorInterplanetario {
         boolean esPalindromo = verificarPalindromo(mensaje);
         System.out.println("¿El mensaje es un palíndromo (integridad del mensaje)?: " + esPalindromo);
     }
+
+class MensajeVacioException extends Exception {
+    public MensajeVacioException(String mensaje) {
+        super(mensaje);
+    }
+}
+
+public static int contarVocales(String mensaje) {
+    int contador = 0;
+    for (int i = 0; i < mensaje.length(); i++) {
+        char c = mensaje.toLowerCase().charAt(i);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            contador++;
+        }
+    }
+    return contador;
+}
+
+public static String invertirMensaje(String mensaje) {
+    return new StringBuilder(mensaje).reverse().toString();
+}
+
+public static boolean verificarPalindromo(String mensaje) {
+    String textoLimpio = mensaje.replaceAll("\\s+", "").toLowerCase();
+    String textoInvertido = new StringBuilder(textoLimpio).reverse().toString();
+    return textoLimpio.equals(textoInvertido);
+}
+}
