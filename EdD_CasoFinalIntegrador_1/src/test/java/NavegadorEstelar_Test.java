caff686b12a33631a36b0212957b1a8827296fd9
jsepucas <jsepucas@myuax.com>
2024-02-25 18:56:58 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/test/java/NavegadorEstelar_Test.java
+++ EdD_CasoFinalIntegrador_1/src/test/java/NavegadorEstelar_Test.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+
 public class NavegadorEstelar_Test {
-}
+
+    @org.junit.Test
+
+    public void testMultiplicacionMatriz() {
+        // Crear y configurar matrices de prueba
+        Matriz a = new Matriz(2, 3);
+        a.establecerValor(0, 0, 1);
+        a.establecerValor(0, 1, 2);
+        a.establecerValor(0, 2, 3);
+        a.establecerValor(1, 0, 4);
+        a.establecerValor(1, 1, 5);
+        a.establecerValor(1, 2, 6);
+
