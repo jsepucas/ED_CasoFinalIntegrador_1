91f1455336145770ee1066044caebd1de9722249
jsepucas <jsepucas@myuax.com>
2024-02-25 18:40:24 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/test/java/PlanificadorDeTareas_Test.java
+++ EdD_CasoFinalIntegrador_1/src/test/java/PlanificadorDeTareas_Test.java
@@ -9,3 +9,20 @@ public class PlanificadorDeTareas_Test {
     private d_PlanificadorDeTareas planificador;
     private List<Tarea> tareas;
 
+    @BeforeEach
+    void setUp() {
+        planificador = new d_PlanificadorDeTareas();
+        tareas = new ArrayList<>();
+
+        tareas.add(new Tarea("Tarea 1", 3));
+        tareas.add(new Tarea("Tarea 2", 2));
+        tareas.add(new Tarea("Tarea 3", 4));
+        tareas.add(new Tarea("Tarea 4", 5));
+
+        MiembroTripulacion miembro1 = new MiembroTripulacion("Juan");
+        MiembroTripulacion miembro2 = new MiembroTripulacion("Lorena");
+
+        planificador.agregarMiembro(miembro1);
+        planificador.agregarMiembro(miembro2);
+    }
+
