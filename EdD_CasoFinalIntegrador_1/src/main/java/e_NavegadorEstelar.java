0f0dd8f6cfcf8b67de8904c24a5fec7a5e9f261c
jsepucas <jsepucas@myuax.com>
2024-02-25 18:53:35 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/main/java/e_NavegadorEstelar.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/e_NavegadorEstelar.java
@@ -1,2 +1,27 @@
-public class e_NavegadorEstelar {
-}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Clase para poder representar y operar con matrices
+class e_NavegadorEstelar
+    private final int[][] datos;
+
+    public Matriz(int filas, int columnas) {
+        datos = new int[filas][columnas];
+    }
+
+    public void establecerValor(int fila, int columna, int valor) {
+        datos[fila][columna] = valor;
+    }
+
+    public int obtenerValor(int fila, int columna) {
+        return datos[fila][columna];
+    }
+
+    public int getFilas() {
+        return datos.length;
+    }
+
+    public int getColumnas() {
+        return datos[0].length;
+    }
+
