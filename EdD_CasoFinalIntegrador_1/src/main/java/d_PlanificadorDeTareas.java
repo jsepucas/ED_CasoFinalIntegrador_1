872f2e18d4071cdff032fc18a0cc9beb52c85688
jsepucas <jsepucas@myuax.com>
2024-02-25 18:31:01 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/main/java/d_PlanificadorDeTareas.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/d_PlanificadorDeTareas.java
@@ -83,3 +83,23 @@ class MiembroTripulacion {
         return nombre;
     }
 }
+
+// Creamos una clase para las tareas
+
+class Tarea {
+    private String nombre;
+    private int duracion;
+
+    public Tarea(String nombre, int duracion) {
+        this.nombre = nombre;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+}
