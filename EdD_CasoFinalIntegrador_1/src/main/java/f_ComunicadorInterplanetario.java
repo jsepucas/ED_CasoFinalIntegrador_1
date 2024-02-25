8988c4786d9d5ac5ccab14a311e52c627d012150
jsepucas <jsepucas@myuax.com>
2024-02-25 19:03:28 +0100

    Commits

--- EdD_CasoFinalIntegrador_1/src/main/java/f_ComunicadorInterplanetario.java
+++ EdD_CasoFinalIntegrador_1/src/main/java/f_ComunicadorInterplanetario.java
@@ -1,2 +1,13 @@
 public class f_ComunicadorInterplanetario {
-}
+
+    // Método para contar vocales en un mensaje
+    public static int contarVocales(String mensaje) {
+        int contador = 0;
+        for (char c : mensaje.toLowerCase().toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
